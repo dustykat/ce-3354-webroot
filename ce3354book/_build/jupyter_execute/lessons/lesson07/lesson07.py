fbeae7eef14fcb38b51f6d03101658f9bcde3318
theodore cleveland <ted.cleveland@gmail.com>
2022-07-26 16:49:51 +0000

    update lesson collection

--- ce3354book/_build/jupyter_execute/lessons/lesson07/lesson07.py
+++ ce3354book/_build/jupyter_execute/lessons/lesson07/lesson07.py
@@ -2,22 +2,15 @@
 # coding: utf-8
 
 # # Evaporation Models
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+
 # ## References
 # 
-# cite pages of textbook
-# 
-# links to other readings 
-# 
-# 
+# 1. [Cleveland, T. G. (2020) *Surface Water Hydrology Notes (Evaporation) to accompany CE-5361*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/Lesson06.pdf)
+# 2. [Blaney-Criddle Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/BlaneyCriddle.xlsx) Right-Click "Save As..."
+# 3. [Thornwaithe Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/Thornwaithe.xls) Right-Click "Save As..."
 
-# 
+# In[ ]:
 
-# 
+
+
+
