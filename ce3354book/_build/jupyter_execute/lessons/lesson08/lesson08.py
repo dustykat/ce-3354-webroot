7d0b3324dd0cac2fa20bf476c9595d363dd0f0fa
theodore cleveland <ted.cleveland@gmail.com>
2022-07-26 21:44:51 +0000

    update literature

--- ce3354book/_build/jupyter_execute/lessons/lesson08/lesson08.py
+++ ce3354book/_build/jupyter_execute/lessons/lesson08/lesson08.py
@@ -3,14 +3,12 @@
 
 # # Infiltration Models
 # 
-# ### Estimating Slope(s)
-# 
+
 # ## References
 # 
-# cite pages of textbook
-# 
-# links to other readings 
-# 
+# 1. [Cleveland, T. G. (2020) *Surface Water Hydrology Notes (Infiltration) to accompany CE-5361*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson07/Lesson07.pdf)
+# 2. [Green-Ampt Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson07/ce5361_green_ampt.xlsx) Right-Click "Save As..."
+
 # 
 
 # 
