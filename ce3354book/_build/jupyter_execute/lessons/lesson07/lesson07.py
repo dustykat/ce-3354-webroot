5493e3b839f21c69e087724db107303dbf7ed776
theodore cleveland <ted.cleveland@gmail.com>
2022-09-23 16:55:56 +0000

    update hms

--- ce3354book/_build/jupyter_execute/lessons/lesson07/lesson07.py
+++ ce3354book/_build/jupyter_execute/lessons/lesson07/lesson07.py
@@ -42,9 +42,10 @@
 
 # ## References
 # 
-# 1. [Cleveland, T. G. (2020) *Surface Water Hydrology Notes (Evaporation) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/Lesson06.pdf)
-# 2. [Blaney-Criddle Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/BlaneyCriddle.xlsx) Right-Click "Save As..."
-# 3. [Thornwaithe Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/Thornwaithe.xls) Right-Click "Save As..."
+# 1. Gupta pp 65-92.
+# 2. [Cleveland, T. G. (2020) *Surface Water Hydrology Notes (Evaporation) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/Lesson06.pdf)
+# 3. [Blaney-Criddle Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/BlaneyCriddle.xlsx) Right-Click "Save As..."
+# 4. [Thornwaithe Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson06/Thornwaithe.xls) Right-Click "Save As..."
 
 # In[ ]:
 
--- ce3354book/_build/jupyter_execute/lessons/lesson08/lesson08.py
+++ ce3354book/_build/jupyter_execute/lessons/lesson08/lesson08.py
@@ -3,14 +3,41 @@
 
 # # Infiltration Models
 # 
+# Examination of various process models at
+# 
+# [Cleveland, T. G. (2020) *Surface Water Hydrology Notes (Infiltration) to accompany CE-5361*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson07/Lesson07.pdf)
+# 
+# ## HEC-HMS Loss Models
+# 
+# >A sub**basin** element conceptually represents infiltration, surface runoff, and
+# subsurface processes interacting together, the actual infiltration calculations are
+# performed by a loss method contained within the subbasin. A total of twelve different
+# loss methods are provided. Some of the methods are designed primarily for
+# simulating events while others are intended for continuous simulation. All of the
+# methods conserve mass. That is, the sum of infiltration and precipitation left on the
+# surface will always be equal to total incoming precipitation.
+# 
+# >The inputs for each loss method are presented on a separate Component Editor
+# from the subbasin element editor. The "Loss" editor is always shown next to the
+# "Subbasin" editor. If the kinematic wave transform method is selected, there may be
+# two loss editors, one for each runoff plane. The information shown on the loss editor
+# will depend on which method is currently selected
+# 
+# A fully provisioned Windows Implementation of HEC-HMS is located at:
+# 
+# - server_name: **kittyinthewindow.ddns.net**
+# - user_name: **texas-skew**
+# - passwd: **peakfq73$hare**
+# 
+# Users must access using Remote Desktop Protocol (Built into Windows, Apple Store has a free Mac application).
+# 
+# - Use the Hardin Creek Project to explore different loss models.
+# 
+# Recomended Loss models for semester project are CN model or Green-Ampt.  These are easiest to parameterize from available data sources for the project and should be adequate for the problem statement.
+# 
+# 
 
 # ## References
 # 
 # 1. [Cleveland, T. G. (2020) *Surface Water Hydrology Notes (Infiltration) to accompany CE-5361*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson07/Lesson07.pdf)
 # 2. [Green-Ampt Spreadsheet (Excel)](http://54.243.252.9/ce-3354-webroot/1-Lectures-2020/lesson07/ce5361_green_ampt.xlsx) Right-Click "Save As..."
-
-# 
-
-# 
-
-# 
