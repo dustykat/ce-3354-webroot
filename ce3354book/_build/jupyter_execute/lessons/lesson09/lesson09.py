b5053f1c937e38053bc521c24ebc69fda1592bfa
theodore cleveland <ted.cleveland@gmail.com>
2022-10-03 19:28:36 +0000

    uh-1

--- ce3354book/_build/jupyter_execute/lessons/lesson09/lesson09.py
+++ ce3354book/_build/jupyter_execute/lessons/lesson09/lesson09.py
@@ -15,16 +15,18 @@
 # The hydrograph pictured also has a baseflow component
 # - Flow in absence of a storm
 # 
+# ## Baseflow Separation
+# 
 # Baseflow separation is a first step in analysis – several methods
 # 
-# ## Constant discharge method
+# ### Constant discharge method
 # 
 # ![](constantdischarge.png)
 # 
 # - When rising limb starts – declare that value to constant rate during the event, rejoin as recession limb.
 # - All flow above the value is declared storm flow
 # 
-# ## Constant slope method
+# ### Constant slope method
 # 
 # ![](constantslope.png)
 # 
@@ -118,7 +120,9 @@
 # 
 # ### How are they built from data (analysis)?
 # 
-# Will demonstrate by example
+# Will demonstrate several approaches by example (mostly using the same data and different methods)
+# 
+# #### Back-Substitution
 # 
 # ![](convolution2.png)
 # 
@@ -133,27 +137,102 @@
 # - Many instances the system is over-determined; more equations than unknowns and an optimization technique is usually applied
 # 
 # ![](convolution6.png)
+
+# #### Least-Squares
 # 
-# - How are they built when data do not exist (synthesis)?
+# This method is good for over-determined cases, although may produce negative weights - usually these are small in magnitude and late in time, so not a huge issue but do need addressing when they arise.
+# 
+# ![](leastsquares.png)
+# 
+# The least squares approach treats the problem as a regression problem, and fits the weights to the data.  A spreadsheet can implement the technique for smallish problems.
+# 
+# ![](leastsquaresSS.png)
+
+# ## What are Unit Hydrographs used for?
+# 
+# They are useful to predict responses to future storms of correct duration.
+# 
+# ![](UHuse1.png)
+# 
+# A future storm of same duration but different magnitude (similar input sequence)
+# 
+# ![](UHuse2.png)
+# 
+# A future storm of same duration but different magnitude (different input sequence)
+# 
+# ![](UHuse3.png)
+# 
+# A future storm of same duration but different magnitude (different input sequence)
+# 
+# ![](UHuse4.png)
 # 
 # 
+
+# ### Parametric Unit Hydrographs
 # 
-# Pick somewhere in Oklahoma
+# The unit weights can be replaced by a function whose shape is adjusted by one or more parameters, these are called parametric unit hydrographs.
 # 
-# ## Summary
+# ![](parametricUH1.png)
+# 
+# ![](parametricUH2.png)
+# 
+# ![](parametricUH3.png)
+# 
+# ### VALUE OF PARAMETRIC UNIT HYDROGRAPHS
+# - Fewer values to keep track of
+# - Simple extension of time-base
+# - If the parameters can be associated with watershed metrics (Slope, MCL, soil properties, shape, etc.) the resulting model is called a synthetic unit hydrograph
+# - Called synthetic because response can be synthesized from the metrics rather than from analyzing observations (which we may not have in cases of practical interest)
+# 
+# ![](parametricUH4.png)
+# 
+# ### Time-Base Extension
+# 
+# Extending the time base of a UH is needed to accomodate storms of much different length than used in the analysis.  Extension for parametric hydrographs is fairly easy, just extend the matrix as needed.  For classically obtained weights, the S-curve technique as as good as any.  Fortunately most software has parametric UH choices built-in, so this is a non-issue these days unless you are using historical UHs and need to bring them into the 21st century.
+# 
+# ![](timebase1.png)
+# 
+# ![](timebase2.png)
+
+# 
+# 
+# ## Summary concepts
+# 
 # - Unit hydrographs map the excess precipitation signal to the outlet
 # - Base-flow separation isolates the total discharge from the storm-induced discharge
 # - Loss models are implicit; the unit hydrograph maps excess to the outlet
-# - Back-substitution (linear equation) method illustrated.
+# - Back-substitution (linear equation) and Least-Squares analysis method illustrated.
+# - Parametric UH described
+# - Hydrograph Analysis
+#   - Measured rainfall and runoff to infer the transfer function.   Implies: Have **DATA.**
+# - Hydrograph Synthesis
+#   - Physical properties of watershed used to postulate the transfer function. Actual measurements not required – Produces an **ESTIMATE**
+
+# 
+
+# 
 
 # ## References
 # 
 # cite pages of textbook
 # 
-# 2. [Cleveland, T. G. (2017) *Surface Water Hydrology Notes (Unit-Hydrographs Analysis) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture13.pdf)
+# 
+# 2. [Cleveland, T. G. (2015) *Surface Water Hydrology Notes (Unit-Hydrographs Analysis) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture13.pdf)
+# 
+# 3. [Cleveland, T. G. (2017) *Surface Water Hydrology Notes (Unit-Hydrographs in HEC-HMS) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture14.pdf)
+# 
+# 4. [FHWA-NHI-02-001 Highway Hydrology Chapter 6, Section 6.1](http://54.243.252.9/ce-3354-webroot/3-Readings/FHWAHighwayHydrology/FHWA-NHI-02-001.pdf)
 # 
 # ## Spreadsheets
-# 1. 
+# 
+# Listed below are spreadsheets that implement simple UH examples.  They are Excel (circa 2009) spreadsheets, that work in current Excel, LibreOffice, and Numbers environments
+# 1. [ExampleUH_BackSub1.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_BackSub1.xls)
+# 2. [ExampleUH_BackSub2.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_BackSub2.xls)
+# 3. [ExampleUH_LeastSquares.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_LeastSquares.xls)
+# 4. [ExampleUH_TransferFn.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_TransferFn.xls)
+# 5. [ExtendedBase_DifferentStorm.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExtendedBase_DifferentStorm.xls)
+# 6. [ExtendedBase.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExtendedBase.xls)
+# 
 
 # 
 
--- ce3354book/_build/jupyter_execute/lessons/lesson10/lesson10.py
+++ ce3354book/_build/jupyter_execute/lessons/lesson10/lesson10.py
@@ -3,51 +3,70 @@
 
 # # Unit Hydrographs - II (Synthesis)
 # 
-# Some definitions of a watershed include:
+# Synthesis does not use rainfall-runoff data.
 # 
-# - Topographic area that collects and discharges surface streamflow through one outlet or mouth (pour point)
-# - The area on the surface of the Earth that drains to a specific location
-# - In groundwater a similar concept is called a groundwater basin – only the boundaries can move depending on relative rates of recharge and discharge 
+# - Uses measurements on the watershed to postulate parameters of a parametric unit hydrograph.
 # 
-# The topographic definition omits that there could be subsurface sewer systems that can cross topographic boundaries.   
-# It’s a big deal in urban areas.
+# ## Parametric Unit Hydrographs (in HEC-HMS)
 # 
-# [insert some images]
+# HEC-HMS has several different UH models available (eg. NRCS DUH, Clark, etc.).
+# These models are described by “parameters” 
+# - NRCS (Tp or Tc)
+# - Clark (Tc, R)
+# Selection of the parameters selects the shape of the UH and the time base (or time to peak).
+# The analyst can also enter an empirical (user specified) unit hydrograph.
 # 
-# ## Watershed Delineation
+# Useful parametric models along with their parameters include:
 # 
-# Define
+# - NRCS DUH : Tlag (Timing only)
+# - Clark : Tc, and R (Timing and a storage-delay)
+# - Snyder : Tlag, Cp (Timing and a peak rate factor)
+# - Gamma : (User-Specified) Tc, K (Timing and a shape factor)
 # 
-# ### By-hand
+# :::{note}
+# Aalysis uses data (rainfall and runoff observations) to develop unit hydrograph models. Choices include:
 # 
-# Example
+# Follow FHWA methods 
+# - Essentially back-substitution or OLS
 # 
-# ### Automated
+# Use HEC-HMS and parametric UH, adjust values of parameters to fit observed runoff
+# - Less tedious than back-substitution or OLS
 # 
+# Use HEC-HMS and a user supplied UH, adjust values of UH ordinates (and re-scale to maintain a unit response) to fit observed runoff
+# - Less tedious than back-substitution or OLS
+# :::
 # 
-# ### Stream Stats Tool
+# ## Example
 # 
-# Pick somewhere in Oklahoma
+# Work the example(s) below:
 # 
-# ## Watershed Metrics
+# [Cleveland, T. G. (2017) *Surface Water Hydrology Notes (Unit-Hydrographs in HEC-HMS) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering. Go to Slide 32](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture14.pdf)
 # 
-# ### Measuring Area
-# ### Measuring Length(s) 
-# ### Estimating Slope(s)
+# A fully provisioned Windows Implementation of HEC-HMS is located at:
 # 
+# - server_name: **kittyinthewindow.ddns.net**
+# - user_name: **texas-skew**
+# - passwd: **peakfq73$hare**
 # 
+# Users must access using Remote Desktop Protocol (Built into Windows, Apple Store has a free Mac application).
 # 
-# 
+# In this case use the DES606-EX6 directory
 # 
 # 
 # ## References
 # 
-# cite pages of textbook
+# 1. Gupta pp 343-372
 # 
-# links to other readings 
 # 
+# 
+# 
+
 # 
 
 # 
 
-# 
+# In[ ]:
+
+
+
+
