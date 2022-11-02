b61d1297835aa32be153423c7fd3059eaa0ed5f6
theodore cleveland <ted.cleveland@gmail.com>
2022-11-02 15:48:06 +0000

    add gw

---
+++ ce3354book/_build/jupyter_execute/lessons/groundwater/groundwater3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Transient Groundwater Flow
+# 
+# ## Topics
+# 
+# - Steady flow to well
+#   - Confined
+#   - Unconfined
+# - Superposition to represent
+#   - Multiple wells
+#   - Aquifer Boundaries
+# 
+# 
+# ## Presentation
+# 
+# Go through slides 1-21 below:
+# 
+# 1. [Cleveland, T. G. (2017) *Engineering Hydrology Notes (Groundwater Hydrology) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture22.pdf)
+# 
+# ## Scripts/Spreadsheets
+# 
+# 1. Theim Model (to show interference effects and map a regional gradient onto a drawdown distribution) [WELL_STY.XLS ](http://54.243.252.9/ce-3354-webroot/ce3354book/lessons/groundwater/WELL_STY.XLS)
+# 
+# 
+# ## References
+# 
+# 1. Gupta pp. 133-151
+# 
+# 2. [Basic Groundwater Hydrology - USGS WSP 2020](http://54.243.252.9/ce-3354-webroot/3-Readings/USGS-WSP2020-GroundwaterHydrology/usgs-wsp-2020.pdf)
+# 
+# 
+
+# 
+
+# 
