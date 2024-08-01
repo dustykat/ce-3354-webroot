290d9e29d7af9ae55ada4a0dd2d16d4b6fb6a800
theodore cleveland <ted.cleveland@gmail.com>
2024-08-01 20:04:48 +0000

    update unit hydrograph notes with images

--- ce-3354-webbook-2024/my3354notes/_build/jupyter_execute/lessons/14-UnitHydrographs/UnitHydrographs.py
+++ ce-3354-webbook-2024/my3354notes/_build/jupyter_execute/lessons/14-UnitHydrographs/UnitHydrographs.py
@@ -2,8 +2,70 @@
 # coding: utf-8
 
 # # 14. Unit Hydrographs - I (Analysis)
+
+# :::{admonition} Course Website
+# [link to webster](http://54.243.252.9/ce-3354-webroot/)
+# :::
 # 
-# A hydrograph is a plot (or paired time-discharge values) of discharge versus time for a location (on a stream).
+# 
+
+# ## Readings
+# 
+# 1. [Gupta, R.S., 2017. Hydrology and Hydraulic Systems, pp. 337-361](https://www.waveland.com/browse.php?t=384)
+# 
+# 2. [Chow, V.T., Maidment,D.M., and Mays, L.W. (1998) Applied Hydrology, McGraw Hill, pp. 201-221](http://54.243.252.9/ce-3354-webroot/3-Readings/CMM1988/) 
+# 
+# 3. [McCuen, R.H., Johnson, P.A., and Ragan, R.M. (2002) Highway Hydrology. HDS-2 (2ed) FHWA-NHI-02-001 (Read Chapter 6, Section 6.1)](http://54.243.252.9/ce-3354-webroot/3-Readings/FHWAHighwayHydrology/FHWA-NHI-02-001.pdf)
+# 
+# 4. [Texas Hydraulic Design Manual (2014-1) Texas Department of Transportation. (2014) Hydraulic Design Manual (Read pages XX-X to XX-X)](http://54.243.252.9/ce-3354-webroot/3-Readings/TXDOT-HYDM-2014/txdot-hdm-2014.pdf)
+# 
+# 5. [Linear Theory of Hydrologic Systems Dooge, J.C. I. (1973) Linear Theory of Hydrologic Systems. USDA ARS Technical Bulletin No. 1468. (Read pages XXX to XXX)](http://54.243.252.9/ce-3354-webroot/3-Readings/LS1973/linear-systems-hydrology-dooge.pdf)
+# 
+# 6. [Cleveland, T. G. (2015) *Surface Water Hydrology Notes (Unit-Hydrographs Analysis) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture13.pdf)
+# 
+# 7. [Cleveland, T. G. (2017) *Surface Water Hydrology Notes (Unit-Hydrographs in HEC-HMS) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture14.pdf)
+
+# ## Videos
+# 
+# 1. [Hydrographs](https://www.youtube.com/watch?v=FGdy_xIaH_Y)
+# 2. [How to Interpret a Storm Hydrograph](https://www.youtube.com/watch?v=uWDP9vl13cU)
+# 3. [Unit Hydrographs Part 1](https://www.youtube.com/watch?v=aDaOFf0IrTE)
+# 4. [Unit Hydrographs Part 2](https://www.youtube.com/watch?v=4f6Hs666dUY)
+# 5. [Baseflow Separation](https://www.youtube.com/watch?v=Qw_oPqPhJLs)
+# 6. [Baseflow Recession and Hydrologic Equation](https://www.youtube.com/watch?v=olectEAy0Z8)
+# 7. [Hydrograph Recession](https://www.youtube.com/watch?v=CykDj1SGGF8&list=PLOO0MwGL5EDLXNfeyRrsj9YZhYk-_BGmD&index=42)
+# 8. [Unit Hydrograph and Proportionality](https://www.youtube.com/watch?v=XofeIB1v7s0)
+# 9. [Design Storm Excess Precipitation Method](https://www.youtube.com/watch?v=Kr4jnIulaiU)
+# 10. [S-Hydrograph (for Time-Shifting)](https://www.youtube.com/watch?v=wSvFrX48JOo)
+# 
+
+# ## Spreadsheets
+# 
+# Listed below are spreadsheets that implement simple UH examples.  They are Excel (circa 2009) spreadsheets, that work in current Excel, LibreOffice, and Numbers environments
+# 1. [ExampleUH_BackSub1.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_BackSub1.xls)
+# 2. [ExampleUH_BackSub2.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_BackSub2.xls)
+# 3. [ExampleUH_LeastSquares.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_LeastSquares.xls)
+# 4. [ExampleUH_TransferFn.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_TransferFn.xls)
+# 5. [ExtendedBase_DifferentStorm.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExtendedBase_DifferentStorm.xls)
+# 6. [ExtendedBase.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExtendedBase.xls)
+# 
+
+# ## Outline
+# 
+# - Hydrographs 
+# - Baseflow
+#  - Separation Techniques
+# - Unit Hydrographs
+#  - Analysis (implies have paired data)
+#  - Back-Substitution
+#  - OLS Regression
+#  - Parametric Unit Hydrographs
+#  - Some examples
+
+# ## Hydrographs
+# 
+# A hydrograph is a graphical (or tabular) representation that shows how the flow rate of water in a river or stream changes over time at a specific location. In engineering hydrology, hydrographs are used to analyze and predict the response of a watershed to precipitation events. They typically display the rate of discharge (usually in cubic meters per second) on the vertical axis and time on the horizontal axis. Key features of a hydrograph include the rising limb, which represents the increase in flow following a precipitation event; the peak discharge, which is the maximum flow rate; and the recession limb, which indicates the decrease in flow as water drains away. Hydrographs are essential tools for designing flood control measures, managing water resources, and understanding watershed behavior.
+# His a plot (or paired time-discharge values) of discharge versus time for a location (on a stream).
 # 
 # ![](hydrograph-1.png)
 # 
@@ -14,28 +76,37 @@
 # - Inflection point
 # The hydrograph pictured also has a baseflow component
 # - Flow in absence of a storm
+
+# ## Baseflow 
 # 
-# ## Baseflow Separation
+# **Baseflow** refers to the portion of streamflow that comes from groundwater or other long-term sources, which sustain the flow in a river during dry periods.
 # 
-# Baseflow separation is a first step in analysis – several methods
+# **Direct runoff** or stormflow represents the portion of streamflow generated by a precipitation event, including surface runoff, interflow, and quick subsurface flow.
 # 
-# ### Constant discharge method
+# **Baseflow separation** is a process used in hydrology to distinguish between the baseflow and the direct runoff components of streamflow in a hydrograph.
+# 
+# When analyzing a hydrograph, baseflow separation is crucial for isolating the direct runoff response from the baseflow, as this separation helps to accurately determine the watershed's response to rainfall events. The baseflow is typically represented as a relatively stable, lower flow rate, while the direct runoff appears as a spike or peak following a precipitation event. By separating these components, hydrologists can better understand the immediate impact of rainfall on streamflow, which is essential for flood forecasting and water resource management.
+# 
+# ### Baseflow separation methods
+# 
+# Methods to estimate/stipulate baseflow are:
+# 
+# #### Constant discharge method
 # 
 # ![](constantdischarge.png)
 # 
 # - When rising limb starts – declare that value to constant rate during the event, rejoin as recession limb.
 # - All flow above the value is declared storm flow
 # 
-# ### Constant slope method
+# #### Constant slope method
 # 
 # ![](constantslope.png)
 # 
 # - When rising limb starts – draw a segment from that value to the inflection point on the recession limb
-# - All flow above the value is
-# declared storm flow
+# - All flow above the value is declared storm flow
 # - Hard to implement for multiple peak hydrographs (real hydrographs may exhibit many peaks)
 # 
-# ### Concave method
+# #### Concave method
 # 
 # ![](concave.png)
 # 
@@ -45,19 +116,28 @@
 # - Hard to implement for multiple peak hydrographs (real hydrographs exhibit many peaks)
 # 
 # There are a few more ways to accomplish baseflow separation
-# - The master-depletion curve method is outlined in the readings
+# - The master-depletion curve method is outlined in the readings (RSG pp. 345)
 # - For many practical cases with multiple peaked hydrographs the constant discharge method is probably the most straightforward to apply (or use continuous simulation techniques – outside scope this course)
+
+# ## Unit Hydrographs
+# 
+# A unit hydrograph is a specific type of hydrograph used in hydrology to represent the response of a watershed to a unit of rainfall excess over a given duration. It is a theoretical tool that helps in understanding how a watershed responds to a uniform rainfall event, usually 1 cm of runoff over the entire basin, occurring over a specified time period, such as an hour or a day.
+# 
+# In a unit hydrograph, the vertical axis represents the flow rate (typically in cubic meters per second), while the horizontal axis represents time. The shape of the unit hydrograph is determined by the physical characteristics of the watershed, such as its size, shape, slope, and land use. It is an essential tool for hydrologists because it allows them to predict the streamflow response to any given rainfall event by scaling and combining the unit hydrographs for different rainfall inputs. This helps in flood forecasting, water resource management, and the design of hydraulic structures.
+# 
+# To construct a unit hydrograph, it is important to isolate the direct runoff portion of the hydrograph from the total streamflow. This means separating the baseflow so that only the runoff generated by the rainfall event is considered. Baseflow separation ensures that the unit hydrograph reflects only the direct runoff, which is critical for the correct scaling and summation of unit hydrographs when predicting the streamflow response to lare or multiple rainfall events.
 # 
 # 
-# ## What is a unit hydrograph?
+# :::{note}
+# The unit hydrograph is a **response/transfer function** that transforms the distributed input to the outlet, accounting for transit time to get from remote parts of the watershed to the outlet.
 # 
-# - Used to explain the time re-distribution of excess precipitation on a watershed
-# - Represents the response of the watershed at the outlet to a unit depth of EXCESS precipitation
-# - EXCESS implies some kind of loss model is applied to the raw precipitation
+# - Explains the temporal re-distribution of excess precipitation on a watershed
+# - Models the response of the watershed at the outlet to a unit depth of **excess** precipitation
+# - **excess** implies some kind of loss model is applied to the raw precipitation
 # - Time re-distribution implies some kind of transfer behavior is applied
 # - [L. K. Sherman 1932](http://54.243.252.9/ce-3354-webroot/3-Readings/ShermanUH/Sherman_ENR_1932.pdf) is credited with seminal publication of the concept
 # 
-# The unit hydrograph is a response function that transforms the distributed input to the outlet, accounting for transit time to get from remote parts of the watershed to the outlet.
+# A pictoral way to conceptualize a unit hydrograph and rainfall runoff in general is to consider the watershed as a signal processor that converts the input signal (hyetograph) into an output signal (hydrograph).  The processor discards some of the signal (losses); delays arrival of some of the signal at the output because of travel distances (transformation); and delays another portion by storing the signal for later use (sotrage).
 # 
 # ![](unithydrograph1.png)
 # 
@@ -169,8 +249,20 @@
 # 
 
 # ### Parametric Unit Hydrographs
+# The unit weights frm above can be replaced by a function whose shape is adjusted by one or more parameters, these are called parametric unit hydrographs.
 # 
-# The unit weights can be replaced by a function whose shape is adjusted by one or more parameters, these are called parametric unit hydrographs.
+# :::{note}A parametric unit hydrograph is a mathematical representation of a unit hydrograph that uses a set of parameters to describe its shape and characteristics. Unlike empirical unit hydrographs, which are derived directly from observed data, parametric unit hydrographs are defined by mathematical functions with specific parameters that can be adjusted to match the observed hydrological response of a watershed.
+# 
+# Common types of parametric unit hydrographs include:
+# 
+# - Gamma Distribution Unit Hydrograph: This uses the gamma distribution function to describe the time-to-peak, peak flow rate, and shape of the hydrograph. Parameters such as shape, scale, and time-to-peak are used to define the distribution.
+# 
+# - Nash Cascade Model: This model assumes that the watershed can be represented as a series of linear reservoirs, each with a constant storage coefficient. The parameters include the number of reservoirs (N) and the storage coefficient (K), which together define the shape and timing of the hydrograph.
+# 
+# - Synthetic Unit Hydrographs: These are constructed using standard shapes like triangular or dimensionless unit hydrographs, with parameters such as time-to-peak, peak discharge, and recession constant used to describe the hydrograph's shape.
+# :::
+# 
+# Parametric unit hydrographs are useful because they provide a flexible and efficient way to model the hydrological response of watersheds, especially in situations where direct measurement data may be limited or unavailable. 
 # 
 # ![](parametricUH1.png)
 # 
@@ -178,13 +270,15 @@
 # 
 # ![](parametricUH3.png)
 # 
+# 
+# 
+# ![](parametricUH4.png)
 # ### VALUE OF PARAMETRIC UNIT HYDROGRAPHS
 # - Fewer values to keep track of
 # - Simple extension of time-base
 # - If the parameters can be associated with watershed metrics (Slope, MCL, soil properties, shape, etc.) the resulting model is called a synthetic unit hydrograph
 # - Called synthetic because response can be synthesized from the metrics rather than from analyzing observations (which we may not have in cases of practical interest)
 # 
-# ![](parametricUH4.png)
 # 
 # ### Time-Base Extension
 # 
@@ -207,37 +301,3 @@
 #   - Measured rainfall and runoff to infer the transfer function.   Implies: Have **DATA.**
 # - Hydrograph Synthesis
 #   - Physical properties of watershed used to postulate the transfer function. Actual measurements not required – Produces an **ESTIMATE**
-
-# 
-
-# 
-
-# ## References
-# 
-# cite pages of textbook
-# 
-# 
-# 2. [Cleveland, T. G. (2015) *Surface Water Hydrology Notes (Unit-Hydrographs Analysis) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture13.pdf)
-# 
-# 3. [Cleveland, T. G. (2017) *Surface Water Hydrology Notes (Unit-Hydrographs in HEC-HMS) to accompany CE-3354*, Department of Civil, Environmental, and Construction Engineering, Whitacre College of Engineering.](http://54.243.252.9/ce-3354-webroot/1-Lectures-2017/Lecture14.pdf)
-# 
-# 4. [FHWA-NHI-02-001 Highway Hydrology Chapter 6, Section 6.1](http://54.243.252.9/ce-3354-webroot/3-Readings/FHWAHighwayHydrology/FHWA-NHI-02-001.pdf)
-# 
-# ## Spreadsheets
-# 
-# Listed below are spreadsheets that implement simple UH examples.  They are Excel (circa 2009) spreadsheets, that work in current Excel, LibreOffice, and Numbers environments
-# 1. [ExampleUH_BackSub1.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_BackSub1.xls)
-# 2. [ExampleUH_BackSub2.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_BackSub2.xls)
-# 3. [ExampleUH_LeastSquares.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_LeastSquares.xls)
-# 4. [ExampleUH_TransferFn.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExampleUH_TransferFn.xls)
-# 5. [ExtendedBase_DifferentStorm.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExtendedBase_DifferentStorm.xls)
-# 6. [ExtendedBase.xls](http://54.243.252.9/ce-3354-webroot/5-Spreadsheets/ExtendedBase.xls)
-# 
-
-# 
-
-# 
-
-# 
-
-# 
