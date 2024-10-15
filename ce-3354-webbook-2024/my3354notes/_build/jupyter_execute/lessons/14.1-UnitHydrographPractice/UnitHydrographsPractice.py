0e580569dbd535059432c8d367520e60d301f807
theodore cleveland <ted.cleveland@gmail.com>
2024-10-15 15:27:20 +0000

    add UH practice

---
+++ ce-3354-webbook-2024/my3354notes/_build/jupyter_execute/lessons/14.1-UnitHydrographPractice/UnitHydrographsPractice.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Unit Hydrographs (In Class Exercise)
+
+# ## Exercise 1
+# 
+# Determine the direct runoff hydrograph resulting from the rainfall pattern in Figure P2-2(a) using the triangular 1-hr UH given in  Fig. P2-2(b)
+# 
+# ![](Raro001.png)
+
+# Workflow:
+#     
+# 1. UH is for 1-hr **excess precipitation**.
+# 2. Is the hyetograph in 1-hr increments?  
+#   - Yes, proceede
+#   - No, convert supplied hyetograph into equivalent 1-hr increments (or whatever is dictated by the UH)
+# 3. Has the loss function been applied?
+#   - Yes, proceede
+#   - No, apply loss function to each hyetograph increment
+# 4. On suitable chart plot each increments response onto the chart - remember to lag the responses consistent with the increment timing.
+
+# ![](BlankChart.png)
+
+# Consider the watershed below.  Area A and B are identical in size, shape, slope, and channel length. 1-hr UH are supplied fro natural and developed conditions for both areas.
+# 
+# ![](Watershed.png)  ![](UHTable.png)
+# 
+# Determine:
+# 1. Assuming natural conditions for both areas determine the outflow at point 1 if 2.5 in.hr of rain falls for 2 hrs.  Total loss is 1 in.
+# 2. Assume area B is fully developed, area A remains au naturel.  Determine the outflow at point 1 if 2.0 in/hr of **excess rain** falls for 1 hrs.  
+# 3. Repeat the above for both areas fully developed.
+# 
+# 
+
+# ![](BlankChart.png)
+
+# ![](BlankChart.png)
+
+# ![](BlankChart.png)
+
+# In[ ]:
+
+
+
+
